b2d713026b39a524233c49118d37fa81d84854ca
Liam Kirsher <liamk@numenet.com>
2013-09-30 15:28:18 -0700

    Fix typo

--- django_runtime_tags/urls.py
+++ django_runtime_tags/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import url, patterns
 from django.views.generic.base import TemplateView
 
 rtt_test_urlpatterns = patterns('',
-    url(r'^rtt/test/$', TemplateView.as_view("django_runtime_tags/tests.html")),
+    url(r'^rtt/test/$', TemplateView.as_view(template_name="django_runtime_tags/tests.html")),
 )
 
