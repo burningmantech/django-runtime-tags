3e4ec12aaf3f6b9c29b8fc6da225ca73d274d3e2
Liam Kirsher <liamk@numenet.com>
2014-10-25 23:11:16 -0700

    Fix django.conf.urls import for django 1.7

--- django_runtime_tags/urls.py
+++ django_runtime_tags/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, patterns
+from django.conf.urls import url, patterns
 from django.views.generic.base import TemplateView
 
 rtt_test_urlpatterns = patterns('',
