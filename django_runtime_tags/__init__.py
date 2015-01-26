564054c9608acb38f31558a02f2293f27b1c10a2
Liam Kirsher <liamk@numenet.com>
2015-01-26 14:29:37 -0800

    Woops.  Missed this one.

--- django_runtime_tags/__init__.py
+++ django_runtime_tags/__init__.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from models import RuntimeTag
+from django.utils.timezone import now
 
 def runtime_value(tag):
     """ Returns the value of the tag, or None if the tag doesn't exist."""
     try:
-        rtt = RuntimeTag.objects.get(key=tag, valid_start__lt=datetime.now())
+        rtt = RuntimeTag.objects.get(key=tag, valid_start__lt=now())
         return rtt.value
     except RuntimeTag.DoesNotExist:
         return None
 
 def runtime_tags():
-    return RuntimeTag.objects.filter(valid_start__lt=datetime.now()).order_by('key')
+    return RuntimeTag.objects.filter(valid_start__lt=now()).order_by('key')
