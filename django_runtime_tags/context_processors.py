4efd84bad7f547b8376c3fd6d5fa6534ffd1b8aa
Liam Kirsher <liamk@numenet.com>
2015-01-27 12:40:05 -0800

    Missed this one too.

--- django_runtime_tags/context_processors.py
+++ django_runtime_tags/context_processors.py
@@ -3,15 +3,15 @@ Add runtime variables to all pages
 """
 from logging import getLogger
 from pprint import PrettyPrinter
-from datetime import datetime
 from models import RuntimeTag
+from django.utils.timezone import now
 
 log = getLogger('django-runtime-tags')
 
 def add_tags(request):
     """Add template tags defined via Django admin to request context."""
 
-    rt_tags = RuntimeTag.objects.filter(valid_start__lt=datetime.now())
+    rt_tags = RuntimeTag.objects.filter(valid_start__lt=now())
     ctx = dict([(t.key, t.value) for t in rt_tags])
     ctx.update({'RUNTIME_TAGS':rt_tags})
 
